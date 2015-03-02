8e46a29c9c4e768bc489b793edc07660f43e8809
Ken <ken@kenhaines.net>
2015-03-01 22:36:38 -0800

    Reorganizing demo/test app under the demo folder to keep the root level cleaner

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 
-app.use(express.static(__dirname ));
+app.use("/node_modules", express.static(__dirname+"/node_modules"));
+app.use("/dist", express.static(__dirname+"/dist"));
+app.use(express.static(__dirname + "/demo" ));
 
 
 
