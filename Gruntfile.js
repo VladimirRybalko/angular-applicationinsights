b580a68af4b212c8faeb3ddff50469c52084f69d
tutujohngeorge <tutujohngeorge@live.com>
2017-03-22 13:58:07 -0400

    Grunt file changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,12 +66,12 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-strip-code');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
- // grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-uglify');
- // grunt.loadNpmTasks('grunt-karma-coveralls');
+  grunt.loadNpmTasks('grunt-karma-coveralls');
 
   
-  grunt.registerTask('default', ['ts', 'concat', 'strip_code', 'uglify']);
+  grunt.registerTask('default', ['ts', 'karma', 'concat', 'strip_code', 'uglify']);
   grunt.registerTask('travis',['ts','karma','concat','strip_code','uglify','coveralls']);
 
 };
